67e7f97647b7900bd94f3eda1da9248ffa023b7e
qrapidnow <qrapidnow@gmail.com>
2024-07-09 18:47:54 +0530

    updated route

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ app.use(bodyParser.json());
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
-  .catch(err => console.log(err));
+  .catch(err => console.log('MongoDB connection error:', err));
+
 
 // Routes
 app.use(orderRoute);  // Ensure the route is prefixed with /api
