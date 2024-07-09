ab44fa5bdb3de2b7463062cbb9386f72bd2c0302
qrapidnow <qrapidnow@gmail.com>
2024-07-09 17:28:39 +0530

    new

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
   .catch(err => console.log(err));
 
 // Routes
-app.use('/api/orders', require('./routes/orders'));
+app.use('/orders', require('./routes/orders'));
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
