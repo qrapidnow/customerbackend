d9524b94fe8df5e2a82d9b8120f216ede0cfcc81
qrapidnow <qrapidnow@gmail.com>
2024-07-10 13:07:59 +0530

    menu

--- routes/order.router.js
+++ routes/order.router.js
@@ -3,6 +3,6 @@ const { saveOrder } = require('../controllers/order.controller');
 const router = express.Router();
 
 // Route to save order
-router.post('orders', saveOrder);
+router.post('/orders', saveOrder);
 
 module.exports = router;
